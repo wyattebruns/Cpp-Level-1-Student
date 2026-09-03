37f85c943454f47edfcad981166c9c83ff9aecb1
wyattebruns <wyattebruns@gmail.com>
2026-09-03 11:56:59 -0700

    Create 7-2 High-Score Tracker.cpp

---
+++ 7-2 High-Score Tracker.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int number_of_scores;
+    float score;
+    int highest;
+    bool first;
+   
+    cout << "How many scores do you want to use!\n";
+    cin >> number_of_scores;
+
+    float scores[number_of_scores];
+
+    cout << "Put in the scores you want to use\n";
+
+    for(float &val: scores){
+     cin >> val;
+}    
+
+   for(float &val: scores){
+    cout << val<< " ";
+   }
+   
+   cout <<endl;
+   
+   
+    for(float val : scores){
+  if(first || val > highest){
+           highest = val;
+           first = false;
+        }
+
+    }
+ 
+ cout <<  "Your highest score is " << highest;
+ 
+
+
+    return 0;
+}
+
+
+https://onecompiler.com/cpp/4526ymwbv
